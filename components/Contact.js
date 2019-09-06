28f96396778dcd19613ff015d57708345a2a5040
Agnieszka Szymlak <agniepiech@gmail.com>
2019-09-06 17:20:56 +0200

    Add files index.html script.js style.css components/

---
+++ components/Contact.js
@@ -0,0 +1,21 @@
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+      React.createElement('div', {className: 'contactItem'},
+        React.createElement('img', {
+          className: 'contactImage',
+          src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiasajo6RFSTR2RkHQrezDFvJ9QY85uHTkci7cpPof5hZSvuiq7g'
+        }),
+        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+          this.props.item.email
+        )
+      )
+    )
+  },
+});
